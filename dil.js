9cf719cc5a81620473e309c6d28ec029ab39f81f
Zehra Karakaya <121049907+0ZeKa0@users.noreply.github.com>
2024-07-04 06:13:29 +0300

    Update dil.js

--- dil.js
+++ dil.js
@@ -3,12 +3,8 @@ let defaultdil = 'tr';
 const dil = {
     tr: {
         baslik: "Zehra Karakaya",
-        aciklama: "Yazılım Mühendisi",
         hakkimda: "Hakkımda",
         hakkimdaprag1:"Merhaba! Ben Zehra Karakaya. Lisans eğitimimi Atatürk Üniversitesi Bilgisayar Mühendisliği bölümünde tamamladım ve o zamandan beri çeşitli projelerde yer aldım. Web geliştirmeye olan tutkum, nesnelerin interneti (IoT) dünyasına olan ilgimle birleşti. IoT, teknolojinin sınırlarını zorlama ve günlük yaşamı daha akıllı hale getirme potansiyeliyle beni derinden etkiliyor.",
-        hakkimdaprag2:"Bu alanda aldığım eğitimler ve gerçekleştirdiğim projeler, bu heyecan verici teknolojiyle nasıl etkileşimde bulunduğumu gösteriyor. Hem donanım hem de yazılım alanlarında kendimi sürekli geliştirmeye odaklanarak, IoT projelerinde benzersiz çözümler üretmeye çalışıyorum.",
-        hakkimdaprag3:"Gömülü IoT sistemlerini ve IoT güvenliğini içeren çeşitli eğitimler aldım ve bu süreçte M2M ve Otonom araçlarda IoT kullanımı projelerini gerçekleştirdim. Her bir proje, IoT'nin gücünü kullanarak nasıl sorunlara çözüm getirebileceğimizi keşfetme fırsatı sağladı.",
-        hakkimdaprag4:"Gelecekte, nesnelerin internetiyle ilgili daha fazla yenilikçi projeye katılmak ve bu alanda derinlemesine çalışmak istiyorum. Eğer benimle aynı tutkuyu paylaşıyorsanız veya birlikte çalışabileceğimiz projeler biliyorsanız, lütfen zehrakarakayabm@gmail.com üzerinden bana ulaşın.",
         projeler:"Projeler",
         port:"Portföy Web Sitesi",
         portdef1:"Kişisel portföy web sitem, benim dijital varlığımı sergilemek ve benimle ilgilenenlerle bağlantı kurmak için tasarlandı. Bu projeyi oluştururken, modern tasarım ilkelerini ve kullanıcı deneyimini öncelikli kılmaya çalıştım.",
@@ -34,12 +30,8 @@ const dil = {
     },
     en: {
         baslik: "Zehra Karakaya",
-        aciklama: "Software Engineer",
         hakkimda: "About me",
         hakkimdaprag1:"Hello! I am Zehra Karakaya. I completed my undergraduate education at Atatürk University Computer Engineering department and since then I have been involved in various projects. My passion for web development has merged with my interest in the world of the internet of things (IoT). IoT fascinates me deeply with its potential to push the boundaries of technology and make everyday life smarter.",
-        hakkimdaprag2:"The training I received and the projects I carried out in this field demonstrate how I interact with this exciting technology. By focusing on continuous improvement in both hardware and software fields, I try to produce unique solutions in IoT projects.",
-        hakkimdaprag3:"I received various trainings including embedded IoT systems and IoT security, and during this period, I carried out projects on the use of IoT in M2M and Autonomous vehicles. Each project provided an opportunity to explore how we can solve problems using the power of IoT.",   
-        hakkimdaprag4:"In the future, I would like to participate in more innovative projects related to the Internet of Things and study in depth in this field. If you share the same passion as me or know projects we can work on together, please contact me via zehrakarakayabm@gmail.com.",
         projeler:"Projects",
         port:"Portfolio Website",
         portdef1:"My personal portfolio website was designed to showcase my digital presence and connect with those interested in me. When creating this project, I tried to prioritize modern design principles and user experience.",
